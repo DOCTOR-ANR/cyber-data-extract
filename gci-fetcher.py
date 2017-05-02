e115c3deb290091b0808989581536453afa07d54
Théo Combe <theo.combe@thalesgroup.com>
2017-05-02 10:58:12 +0200

    Possibility to carry GCI input file in the container for integration tests

--- gci-fetcher.py
+++ gci-fetcher.py
@@ -11,6 +11,7 @@ cybercaptor_file = "/tmp/cybercaptor-input.xml"
 
 parser = argparse.ArgumentParser(description='Converts GCI file to CyberCAPTOR file')
 parser.add_argument('--config', dest='config_file', required=True, help='The config file.')
+parser.add_argument('--mode', dest='mode', required=True, help='The mode : Local takes input file from a local file, remote takes input file from the url in the config file. (Default remote)')
 args = parser.parse_args()
 
 with open(args.config_file) as f:
@@ -23,16 +24,19 @@ if delay < 2:
 while True:
     ok = True
     try:
-
-        # fetch GCI file
-        request_gci = requests.get(config['gci_url'])
-        if request_gci.status_code != 200:
-            print "Got status code %d for GCI request" % request_gci.status_code
-            ok = False
+	if args.mode != "local":
+        	# fetch GCI file
+        	request_gci = requests.get(config['gci_url'])
+        	if request_gci.status_code != 200:
+            		print "Got status code %d for GCI request" % request_gci.status_code
+            		ok = False
+		else:
+			with open(gci_file, 'w') as f:
+				f.write(request_gci.text)
         else:
-            with open(gci_file, 'w') as f:
-                f.write(request_gci.text)
-                
+		subprocess.check_call(["cp", config['gci_local_file'], gci_file])
+
+        if ok == True:
             # convert input
             subprocess.check_call(["/usr/bin/python", "main.py", "--gci-file", gci_file, "--to-fiware-xml-topology", cybercaptor_file])
             
